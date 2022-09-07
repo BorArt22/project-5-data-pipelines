3680531e012f720fc972c83a2566230b9aecc65d
BorArt22 <borart22@gmail.com>
2022-09-07 14:10:05 +0200

    add file for create a cloud datawehouse

---
+++ airflow/dags/dag.py
@@ -0,0 +1,97 @@
+from datetime import datetime, timedelta
+import os
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
+                                LoadDimensionOperator, DataQualityOperator)
+
+default_args = {
+    'owner': 'udacity',
+    'depends_on_past': False,
+    'start_date': datetime(2018, 11, 1),
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'catchup_by_default': False,
+    'email_on_retry': False
+}
+
+dag = DAG('dag',
+          default_args=default_args,
+          description='Load and transform data in Redshift with Airflow',
+          schedule_interval='0 0 * * *'
+        )
+
+start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+
+stage_events_to_redshift =  StageToRedshiftOperator(
+    task_id='Stage_events',
+    dag=dag,
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    target_table="public.staging_events",
+    s3_bucket="udacity-dend",
+    s3_key="log_data",
+    json_paths="log_json_path.json",
+    use_partitioned_data="True",
+    execution_date="{{ yesterday_ds }}"
+)
+
+
+stage_songs_to_redshift =  StageToRedshiftOperator(
+    task_id='Stage_songs',
+    dag=dag,
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    target_table="public.staging_songs",
+    s3_bucket="udacity-dend",
+    s3_key="song_data",
+    json_paths="",
+    use_partitioned_data="True",
+    execution_date="{{ yesterday_ds }}"
+)
+
+load_songplays_table = LoadFactOperator(
+    task_id='Load_songplays_fact_table',
+    dag=dag
+)
+
+load_user_dimension_table = LoadDimensionOperator(
+    task_id='Load_user_dim_table',
+    dag=dag
+)
+
+load_song_dimension_table = LoadDimensionOperator(
+    task_id='Load_song_dim_table',
+    dag=dag
+)
+
+load_artist_dimension_table = LoadDimensionOperator(
+    task_id='Load_artist_dim_table',
+    dag=dag
+)
+
+load_time_dimension_table = LoadDimensionOperator(
+    task_id='Load_time_dim_table',
+    dag=dag
+)
+
+run_quality_checks = DataQualityOperator(
+    task_id='Run_data_quality_checks',
+    dag=dag
+)
+
+end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+start_operator >> stage_songs_to_redshift
+start_operator >> get_execution_time_operator >> stage_events_to_redshift
+
+stage_songs_to_redshift >> load_artist_dimension_table >> load_song_dimension_table
+
+stage_events_to_redshift >> load_time_dimension_table
+stage_events_to_redshift >> load_user_dimension_table
+
+load_song_dimension_table >> load_songplays_table
+load_time_dimension_table >> load_songplays_table
+load_user_dimension_table >> load_songplays_table
+
+load_songplays_table >> run_quality_checks >> end_operator
--- airflow/plugins/operators/stage_redshift.py
+++ airflow/plugins/operators/stage_redshift.py
@@ -1,26 +1,94 @@
+import datetime
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class StageToRedshiftOperator(BaseOperator):
+    """
+
+    """
     ui_color = '#358140'
+    template_fields = ("s3_key","execution_date")
+
+    sql_template_json = """
+        COPY {}
+        FROM '{}'
+        ACCESS_KEY_ID '{}'
+        SECRET_ACCESS_KEY '{}'
+        format as json {}
+    """
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
+                 # Define operators params (with defaults)
+                 redshift_conn_id="",
+                 aws_credentials_id="",
+                 target_table="",
+                 s3_bucket="",
+                 s3_key="",
+                 json_paths="\'auto\'",
+                 use_partitioned_data="False",
+                 execution_date="",
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+
+        # Map params
+        self.redshift_conn_id = redshift_conn_id
+        self.aws_credentials_id = aws_credentials_id
+        self.target_table = target_table
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.json_paths = json_paths
+        self.use_partitioned_data = use_partitioned_data
+        self.execution_date = execution_date        
 
     def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
+        # Set AWS S3 and Redshift connections
+        self.log.info("Process: Setting up Redshift connection")
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        self.log.info("Redshift connection created.")
 
+        self.log.info("Process: Clearing data from Redshift target table")
+        redshift.run("DELETE FROM {}".format(self.target_table))
 
+        # Prepare S3 paths
+        self.log.info("Process: Preparing Copying data from S3 to Redshift")
+        exec_date_rendered = self.execution_date.format(**context)
+        self.log.info("Execution_date: {}".format(exec_date_rendered))
+        exec_date_obj = datetime.datetime.strptime( exec_date_rendered, \
+                                                    '%Y-%m-%d')
+        self.log.info("Execution_year: {}".format(exec_date_obj.year))
+        self.log.info("Execution_month: {}".format(exec_date_obj.month))
+        self.log.info("Execution_day: {}".format(exec_date_obj.day))
 
+        if self.use_partitioned_data == "True":
+            s3_path = "s3://{}/{}/{}/{}/{}-{}-{:02d}-events.json".format(self.s3_bucket, self.s3_key,
+                                                              exec_date_obj.year, exec_date_obj.month,
+                                                              exec_date_obj.year, exec_date_obj.month, exec_date_obj.day)
+        else:
+            s3_path = "s3://{}/{}".format(self.s3_bucket, self.s3_key)
 
+        if self.json_paths == "":
+            s3_json_path = "\'auto\'"
+        else:
+            s3_json_path = "\'s3://{}/{}\'".format( self.s3_bucket, \
+                                                    self.json_paths)
 
+        self.log.info("S3_PATH: {}".format(s3_path))
+        self.log.info("S3_JSON_PATH: {}".format(s3_json_path))
+
+        # Copy data from S3 to Redshift
+        self.log.info("Preparing for JSON input data")
+        formatted_sql = StageToRedshiftOperator.sql_template_json.format(
+            self.target_table,
+            s3_path,
+            credentials.access_key,
+            credentials.secret_key,
+            s3_json_path
+        )
+
+        # Executing COPY operation
+        self.log.info("Process: Executing Redshift COPY operation")
+        redshift.run(formatted_sql)
+        self.log.info("Redshift COPY operation DONE.")
